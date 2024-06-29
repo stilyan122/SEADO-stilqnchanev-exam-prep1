1a308881364f0ef9c93ffb7d934269306aedfb96
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-06-29 10:03:23 +0300

    Repo fix and port change

--- tests/ui/home.test.js
+++ tests/ui/home.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check home page', async ({ page }) => {
-    await page.goto('http://localhost:8080/');
+    await page.goto('http://localhost:8082/');
     const heading = await page.$('h2');
     const text = await heading.textContent();
     expect(text).toContain('My shopping list');  
