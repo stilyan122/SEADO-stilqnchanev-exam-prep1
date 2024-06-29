3fa48794172c6188e49fff71daa511d202fbd468
Stilyan <117260079+stilyan122@users.noreply.github.com>
2024-06-29 10:14:36 +0300

    Fixed UI and Unit tests

--- tests/ui/add-product.test.js
+++ tests/ui/add-product.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add products page', async ({ page }) => {
-    await page.goto('http://localhost:8082/add-to-shopping-list');
-    const form = await page.$('input');
+    await page.goto('http://localhost:8082/add-product');
+    const form = await page.$('form');
     expect(form).toBeTruthy();
   });
   
